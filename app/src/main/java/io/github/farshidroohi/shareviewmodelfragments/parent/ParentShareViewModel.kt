cb9f2c265fbbd8bbad7b011bf69b4b08035c7a10
Farshid Roohi <Farshid.roohi.a@Gmail.com>
2023-08-28 14:59:32 +0330

    rename

--- app/src/main/java/io/github/farshidroohi/shareviewmodelfragments/parent/ParentShareViewModel.kt
+++ app/src/main/java/io/github/farshidroohi/shareviewmodelfragments/parent/ParentShareViewModel.kt
@@ -3,29 +3,27 @@ package io.github.farshidroohi.shareviewmodelfragments.parent
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.SharedFlow
 import kotlinx.coroutines.flow.StateFlow
-import kotlinx.coroutines.flow.asSharedFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.launch
 
 class ParentShareViewModel : ViewModel() {
 
-    private val _mutableSharedFlow = MutableStateFlow(ParentUIState())
-    val uiStateStatedFlow: StateFlow<ParentUIState> = _mutableSharedFlow.asStateFlow()
+    private val _mutableStateFlow = MutableStateFlow(ParentUIState())
+    val uiStateStatedFlow: StateFlow<ParentUIState> = _mutableStateFlow.asStateFlow()
 
     fun increaseValue() {
         viewModelScope.launch {
-            val newNumber = _mutableSharedFlow.value.number + 1
-            _mutableSharedFlow.emit(_mutableSharedFlow.value.copy(number = newNumber))
+            val newNumber = _mutableStateFlow.value.number + 1
+            _mutableStateFlow.emit(_mutableStateFlow.value.copy(number = newNumber))
         }
 
     }
 
     fun reduceValue() {
         viewModelScope.launch {
-            val newNumber = _mutableSharedFlow.value.number - 1
-            _mutableSharedFlow.emit(_mutableSharedFlow.value.copy(number = newNumber))
+            val newNumber = _mutableStateFlow.value.number - 1
+            _mutableStateFlow.emit(_mutableStateFlow.value.copy(number = newNumber))
         }
     }
 
